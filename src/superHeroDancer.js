31de338f34bdd09c276ba6ca6f78872948364139
Hack Reactor Student <student@hackreactor.com>
2017-04-03 20:08:58 -0700

    Day1

--- src/superHeroDancer.js
+++ src/superHeroDancer.js
@@ -1,5 +1,5 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  makeDancer.call(this, top, left, timeBetweenSteps);  
+var superHeroDancer = function(top, left, timeBetweenSteps) {
+	  makeDancer.call(this, top, left, timeBetweenSteps);  
   
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -9,9 +9,9 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
 };
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-makeBlinkyDancer.prototype.step = function() {  
-  makeDancer.prototype.step.call(this)
-  this.$node.toggle();
+	makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+	makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+	makeBlinkyDancer.prototype.step = function() {  
+	  makeDancer.prototype.step.call(this);
+	  this.$node.toggle();
 }
